2b7a299eedb6891de4ca0658bcad78622131b73f
Saleh <hmohamad.sms@gmail.com>
2025-10-30 17:45:16 +0330

    feat: add functionality discount-check

---
+++ src/component/discount-check/discount-check.js
@@ -0,0 +1,27 @@
+export function DiscountCheck() {
+
+
+  const totalOrder = document.querySelector(".total-order");
+  const discount = document.querySelector(".discount");
+  const discountInput = document.getElementById("code");
+  
+
+  switch (discountInput.value) {
+    case "gold":
+      discount.innerText = totalOrder.innerText / 100 * 20;
+      break;
+    case "silver":
+      discount.innerText = totalOrder.innerText / 100 * 15;
+      break;
+    case "bronze":
+      discount.innerText = totalOrder.innerText / 100 * 10;
+      break;
+    default:
+      discount.innerText = 0;
+      discountInput.value = "تخفیف نامعتبر";
+      break;
+  }
+
+  const tax = document.querySelector(".tax");
+  finalPrice.innerText = parseInt(totalOrder.innerText) + parseInt(tax.innerText) - parseInt(discount.innerText);
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { Container } from './component/container-block/container';
 import { AddFood } from './component/add-food/add-food';
 import { RemoveFood } from './component/remove-food/remove-food';
+import { DiscountCheck } from './component/discount-check/discount-check';
 import './style.css'
 
 const app = document.getElementById('app');
@@ -16,3 +17,6 @@ const removeBtn = document.querySelectorAll(".remove");
 removeBtn.forEach(btn => {
     btn.addEventListener("click", RemoveFood);
 });
+
+const confirmBtn = document.querySelector(".confirm");
+confirmBtn.addEventListener("click", DiscountCheck);
