b7e870f1aa51e89c5931c3af3226831c848bc8dc
Saleh <hmohamad.sms@gmail.com>
2025-10-30 17:29:04 +0330

    feat: add functionality add-food btn

---
+++ src/component/add-food/add-food.js
@@ -0,0 +1,26 @@
+export function AddFood(e) {
+    const countFood = e.target.closest(".food-item").querySelector(".food-number");
+    const currentCount = parseInt(countFood.innerText);
+    countFood.innerText = currentCount + 1;
+
+
+    const totalPrice = e.target.closest(".food-item").querySelectorAll("span")[1];
+    const foodPrice = e.target.closest(".food-item").querySelectorAll("span")[0];
+    totalPrice.innerText = foodPrice.innerText * countFood.innerText;
+    
+
+    const totalOrder = document.querySelector(".total-order");
+    const allTotalPrices = document.querySelectorAll(".total-food-price");
+    let sum = 0;
+    allTotalPrices.forEach(price => {
+        sum += parseInt(price.innerText);
+    });
+    totalOrder.innerText = sum;
+    
+
+    const tax = document.querySelector(".tax");
+    tax.innerText = totalOrder.innerText / 100 * 9;
+
+    const discount = document.querySelector(".discount");
+    finalPrice.innerText = parseInt(totalOrder.innerText) + parseInt(tax.innerText) - parseInt(discount.innerText);
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { Container } from './component/container-block/container';
+import { AddFood } from './component/add-food/add-food';
 import './style.css'
 
 const app = document.getElementById('app');
 
 app.append(Container());
+
+const addBtn = document.querySelectorAll(".add");
+addBtn.forEach(btn => {
+    btn.addEventListener("click", AddFood);
+});
